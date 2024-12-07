6b55ca54f62a561f12eda4f7c6e785eaedec4cba
michalwrpo <michal.p.walus@gmail.com>
2024-12-07 18:12:14 +0100

    add board, game and gamemanager

---
+++ server/game/Game.go
@@ -0,0 +1,69 @@
+package game
+
+import (
+	"fmt"
+	"slices"
+)
+
+type Game struct {
+	gameID    int
+	playerNum int
+	players   []string
+	board     Board
+}
+
+func NewGame(gameID, playerNum int, board Board) (*Game, error) {
+	if slices.Contains([]int{2, 3, 4, 6}, playerNum) {
+		game := &Game{
+			gameID:    gameID,
+			playerNum: playerNum,
+			board:     board,
+		}
+		return game, nil
+	} else {
+		return nil, fmt.Errorf("invalid playerNum, allowed: 2, 3, 4, 6")
+	}
+}
+
+func (g *Game) SetPlayerNum(playerNum int) error {
+	allowed := []int{2, 3, 4, 6}
+	if slices.Contains(allowed, playerNum) {
+		if playerNum >= len(g.players) {
+			g.playerNum = playerNum
+			return nil
+		} else {
+			return fmt.Errorf("can't change number of players, more players already in lobby")
+		}
+	} else {
+		return fmt.Errorf("invalid playerNum, allowed: 2, 3, 4, 6")
+	}
+}
+
+func (g *Game) SetBoard(board Board) {
+	g.board = board
+}
+
+func (g *Game) AddPlayer(username string) error {
+	if g.playerNum != 0 {
+		if !slices.Contains(g.players, username) {
+			if len(g.players) < g.playerNum {
+				g.players = append(g.players, username)
+				return nil
+			} else {
+				return fmt.Errorf("lobby full")
+			}
+		} else {
+			return fmt.Errorf("username already in use")
+		}
+	} else {
+		return fmt.Errorf("critical error: Lobby not fully initialized")
+	}
+}
+
+func (g *Game) GetID() int {
+	return g.gameID
+}
+
+func (g *Game) GetBoard() Board {
+	return g.board
+}
---
+++ server/game/GameManager.go
@@ -0,0 +1,36 @@
+package game
+
+type GameManager struct {
+	nextID int
+	games  map[int]*Game
+}
+
+func NewGameManager() *GameManager {
+	gameManager := &GameManager{
+		nextID: 0,
+	}
+	return gameManager
+}
+
+func (gm *GameManager) CreateGame(playerNum int, board Board) (*Game, error) {
+	game, err := NewGame(gm.nextID, playerNum, board)
+	if err == nil {
+		gm.nextID += 1
+		return game, nil
+	} else {
+		return nil, err
+	}
+}
+
+func (gm *GameManager) GetGames() map[int]*Game {
+	return gm.games
+}
+
+func (gm *GameManager) JoinGame(gameID int, username string) error {
+	err := gm.games[gameID].AddPlayer(username)
+	if err == nil {
+		return nil
+	} else {
+		return err
+	}
+}
